9cf5da148258c75f3c1e9986e6048ffa15e0dec3
Alex Tandy <alex.s.tandy@gmail.com>
2012-06-13 15:41:20 -0400

    added background and modifying styles

--- euro_pull.php
+++ euro_pull.php
@@ -7,32 +7,6 @@ $html = file_get_html('http://www.uefa.com/uefaeuro/season=2012/matches/all/inde
 
 
 echo $html->getElementById("md_2_2")->childNodes(1);
-//echo $html->getElementById("div1")->childNodes(1)->childNodes(1)->childNodes(2)->getAttribute('id');
-
-
-
-
-
-/*
-
-
-/*
-
-foreach($html->find('table[class]') as $e)
-	echo $e->plaintext;
-*/
-
-
-
-// Find the DIV tag with an id of "myId"
-/*
-foreach($html->find('div#matchesindex table.date20120613') as $e) 
-    echo $e->plaintext . '<br>';
-
-
-	foreach($html->find('td') as $e)
-	    echo $e->innertext . '<br>';
-*/
 
 
 
