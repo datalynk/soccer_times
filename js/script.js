3ee4c2066776df96e77043b01c9e95fbbd81f963
Alex Tandy <alex.s.tandy@gmail.com>
2012-06-13 21:24:56 -0400

    added twilio library. also added a button on the mainpage that sends a text toa  number. TODO: add form that utilizes a variable to pass through for a name and nnumber.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-/* Author:
+// Event Handlers
 
-*/
+// instantiate AJAX
+var req = new XMLHttpRequest();
+
+$('button.text_alert').click(function() {
+	
+	var submitter_number = "19253300913";
+	var params = "submitter_number="+submitter_number;
+	req.open("POST", "sendsms.php", true);
+	
+	req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	
+	req.onreadystatechange = function() {
+		if(req.readyState == 4 && req.status == 200) {
+			var return_data = req.responseText;
+			$('.response').text(return_data);
+		}
+	}
+	req.send(params);
+});
 
 
 
+// "location.php?lat="+position.coords.latitude
 
 
