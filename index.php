9a97b1c70aaa36bbc031c6d71d13a8a7ca5bb178
Alex Tandy <alex.s.tandy@gmail.com>
2012-06-19 10:37:32 -0400

    fixed soccerball commit error on front page

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <body>
 	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
 	<header>
-		<img class="header_ball" src="img/soccerball.svg"/>
+		<img class="header_ball" src="/img/soccerball.svg"/>
 		<h1>title</h1>
 		<nav class="floatR">
 			<a href="#">Calendar</a>
