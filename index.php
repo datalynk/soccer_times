021bb1d85e87763d30502c981702293ec4d29f3e
Alex Tandy <alex.s.tandy@gmail.com>
2012-06-21 12:54:00 -0400

    fixed minimized media queries for venn diagram... working on mobile

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 				<img class="logo" src="img/soccerlogo.png"/>
 				<img class="bigline" src="img/line.png"/>
 				<nav>
-					<a href="#"><img src="img/e-maillogo.png"/></a>
+					<a href="mailto:alex@newspanion.com?Subject=Hello!"><img src="img/e-maillogo.png"/></a>
 				</nav>
 			</header>
 
@@ -113,13 +113,22 @@
 	<script src="js/plugins.js"></script>
 	<script src="js/script.js"></script>
 
-	<script>
-	 var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-	 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-	 g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-	 s.parentNode.insertBefore(g,s)}(document,'script'));
-	</script>
+	<script type="text/javascript">
 
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-32833497-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </body>
+
+
+
 
 </html>
